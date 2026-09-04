7ebcfe2ac0f20e998d7232a1da41cdd98ebfc1e2
AdrianCardonaCandil <adrian.cardona102@alu.ulpgc.es>
2026-09-04 04:47:02 +0100

    rail segments added

--- backend/tiles/config/01_build_style.py
+++ backend/tiles/config/01_build_style.py
@@ -80,6 +80,8 @@ manifest = [
     'transportation/road/motorway/casing.json',
     'transportation/road/trunk/line.json',
     'transportation/road/motorway/line.json',
+    'transportation/rail/line.json',
+    'transportation/rail/ticks.json',
     'transportation/road/other/label.json',
     'transportation/road/living-street/label.json',
     'transportation/road/residential/label.json',
@@ -87,7 +89,8 @@ manifest = [
     'transportation/road/secondary/label.json',
     'transportation/road/primary/label.json',
     'transportation/road/trunk/label.json',
-    'transportation/road/motorway/label.json'
+    'transportation/road/motorway/label.json',
+    'transportation/rail/label.json'
 ]
 
 # Layers embedding
