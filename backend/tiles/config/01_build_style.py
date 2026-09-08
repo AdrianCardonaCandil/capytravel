88b3de42b779f306b303ef7b9bf318cd2d0b6236
AdrianCardonaCandil <adrian.cardona102@alu.ulpgc.es>
2026-09-08 22:36:49 +0100

    remame style -> layers

--- backend/tiles/config/01_build_style.py
+++ backend/tiles/config/01_build_style.py
@@ -7,7 +7,7 @@ from typing import TypeAlias, cast
 # Route configuration
 base_dir = Path("../")
 style_file = base_dir / 'style.json'
-layers_dir = base_dir / 'style'
+layers_dir = base_dir / 'layers'
 
 # Type definitions
 JSONValue: TypeAlias = (
